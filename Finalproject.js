99743a4fb5bc2e510785b7c2e668a6df9718eaab
Sisam Bhandari <sisambhandari3@yahoo.com>
2015-08-14 09:30:23 -0400

    alsdjf

--- Finalproject.js
+++ Finalproject.js
@@ -1,10 +1,11 @@
 //App with a jar that keeps track of goal
-
+var totalcount=0;
+var count=0;
 function allowDrop(ev) {
     ev.preventDefault();
 }
 
-function drag(ev) {
+/*function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
@@ -13,14 +14,16 @@ function drop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
 }
+*/
+
 function bigImg(x) {
-    x.style.height = "180px";
-    x.style.width = "160px";
+    x.style.height = "140px";
+    x.style.width = "120px";
 }
 
 function normalImg(x) {
-    x.style.height = "120px";
-    x.style.width = "100px";
+    x.style.height = "110px";
+    x.style.width = "90px";
 }
 
 
@@ -33,36 +36,245 @@ function decrease(x) {
     x.style.height = "70px";
     x.style.width = "70px";
 }
-function show(shown, hidden, hidden2) {
+function showPage(shown, hidden, hidden2, hidden3) {
   document.getElementById(shown).style.display='block';
   document.getElementById(hidden).style.display='none';
   document.getElementById(hidden2).style.display='none';
+  document.getElementById(hidden3).style.display='none';
   return false;
+
+
+
+  /* --------------------------------------------todo stuff-------------------*/
 }
+var todo = todo || {},
+    data = JSON.parse(localStorage.getItem("todoData"));
 
-function changeJar(){
+data = data || {};
 
-  
-    if($('#options').val() == "SchoolWork"){
-      alert(schoolWork);
-      //$('#picHolder').attr('src', 'cow.jpeg');
-    }
-    else if($('#options').val() == "Gym"){
-      alert(Gym);
-      //$('#picHolder').attr('src', 'pig.jpg');
-    }
-    else if($('#options').val() == "x"){
-      alert(x);
-     // $('#picHolder').attr('src', 'lamb.jpg');
-    }
-    else{
-      alert(y);
-      //$('#picHolder').attr('src', 'pony.jpg');
-    }
-    
-    
-  // Our Job is going to be adding the photos to this mobile app
-  
-}
+(function(todo, data, $) {
 
-changePicture();
+    var defaults = {
+            todoTask: "todo-task",
+            todoHeader: "task-header",
+            todoDate: "task-date",
+            todoDescription: "task-description",
+            taskId: "task-",
+            formId: "todo-form",
+            dataAttribute: "data",
+            deleteDiv: "delete-div"
+        }, codes = {
+            "1" : "#pending",
+            "2" : "#inProgress",
+            "3" : "#completed"
+        };
+
+    todo.init = function (options) {
+
+        options = options || {};
+        options = $.extend({}, defaults, options);
+
+        $.each(data, function (index, params) {
+            generateElement(params);
+        });
+
+        /*generateElement({
+            id: "123",
+            code: "1",
+            Project: "asd",
+            date: "22/12/2013",
+            description: "Blah Blah"
+        });*/
+
+        /*removeElement({
+            id: "123",
+            code: "1",
+            Project: "asd",
+            date: "22/12/2013",
+            description: "Blah Blah"
+        });*/
+
+        // Adding drop function to each category of task
+        $.each(codes, function (index, value) {
+            $(value).droppable({
+                drop: function (event, ui) {
+                        var element = ui.helper,
+                            css_id = element.attr("id"),
+                            id = css_id.replace(options.taskId, ""),
+                            object = data[id];
+
+                            // Removing old element
+                            removeElement(object);
+
+                            // Changing object code
+                            object.code = index;
+
+                            // Generating new element
+                            generateElement(object);
+
+                            // Updating Local Storage
+                            data[id] = object;
+                            localStorage.setItem("todoData", JSON.stringify(data));
+
+                            // Hiding Delete Area
+                            $("#" + defaults.deleteDiv).hide();
+                    }
+            });
+        });
+
+        // Adding drop function to delete div
+        $("#" + options.deleteDiv).droppable({
+            drop: function(event, ui) {
+                var element = ui.helper,
+                    css_id = element.attr("id"),
+                    id = css_id.replace(options.taskId, ""),
+                    object = data[id];
+
+                // Removing old element
+                removeElement(object);
+
+                // Updating local storage
+                delete data[id];
+                localStorage.setItem("todoData", JSON.stringify(data));
+
+                // Hiding Delete Area
+                $("#" + defaults.deleteDiv).hide();
+            }
+        })
+
+    };
+
+    // Add Task
+    var generateElement = function(params){
+        var parent = $(codes[params.code]),
+            wrapper;
+
+        if (!parent) {
+            return;
+        }
+
+        wrapper = $("<div />", {
+            "class" : defaults.todoTask,
+            "id" : defaults.taskId + params.id,
+            "data" : params.id
+        }).appendTo(parent);
+
+        $("<div />", {
+            "class" : defaults.todoHeader,
+            "text": params.Project
+        }).appendTo(wrapper);
+
+        $("<div />", {
+            "class" : defaults.todoDate,
+            "text": params.date
+        }).appendTo(wrapper);
+
+        $("<div />", {
+            "class" : defaults.todoDescription,
+            "text": params.description
+        }).appendTo(wrapper);
+
+      wrapper.draggable({
+            start: function() {
+                $("#" + defaults.deleteDiv).show();
+            },
+            stop: function() {
+                $("#" + defaults.deleteDiv).hide();
+            },
+          revert: "invalid",
+          revertDuration : 200
+        });
+
+    };
+
+    // Remove task
+    var removeElement = function (params) {
+        $("#" + defaults.taskId + params.id).remove();
+    };
+
+    todo.add = function() {
+        var inputs = $("#" + defaults.formId + " :input"),
+            errorMessage = "Task can not be empty",
+            id, Project, description, date, tempData;
+
+        if (inputs.length !== 4) {
+            return;
+        }
+
+        Project = inputs[0].value;
+        description = inputs[1].value;
+        date = inputs[2].value;
+
+        if (!Project) {
+            generateDialog(errorMessage);
+            return;
+        }
+
+        id = new Date().getTime();
+
+        tempData = {
+            id : id,
+            code: "1",
+            Project: Project,
+            date: date,
+            description: description
+        };
+
+        // Saving element in local storage
+        data[id] = tempData;
+        localStorage.setItem("todoData", JSON.stringify(data));
+
+        // Generate Todo Element
+        generateElement(tempData);
+
+        // Reset Form
+        inputs[0].value = "";
+        inputs[1].value = "";
+        inputs[2].value = "";
+    };
+
+    var generateDialog = function (message) {
+        var responseId = "response-dialog",
+            Project = "Message",
+            responseDialog = $("#" + responseId),
+            buttonOptions;
+
+        if (!responseDialog.length) {
+            responseDialog = $("<div />", {
+                    Project: Project,
+                    id: responseId
+            }).appendTo($("body"));
+        }
+
+        responseDialog.html(message);
+
+        buttonOptions = {
+            "Ok" : function () {
+                responseDialog.dialog("close");
+            }
+        };
+
+      responseDialog.dialog({
+            autoOpen: true,
+            width: 400,
+            modal: true,
+            closeOnEscape: true,
+            buttons: buttonOptions
+        });
+    };
+
+
+    todo.clear = function () {
+        data = {};
+        localStorage.setItem("todoData", JSON.stringify(data));
+        $("." + defaults.todoTask).remove();
+        totalcount-=1;
+    };
+
+})(todo, data, jQuery);
+                   
+    todo.save = function() {
+
+    }
+
+
