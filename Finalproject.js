90d588391dfd7152d9d97b0390c3c0890d8a4800
Sisam Bhandari <sisambhandari3@yahoo.com>
2015-08-18 09:06:19 -0400

    kates final

--- Finalproject.js
+++ Finalproject.js
@@ -1,13 +1,15 @@
 function allowDrop(ev) {
     ev.preventDefault();
 }
-function bigImg(x) {
+function bigImg(x,beans) {
     x.style.height = "140px";
     x.style.width = "120px";
+
 }
-function normalImg(x) {
+function normalImg(x,beans) {
     x.style.height = "120px";
     x.style.width = "100px";
+
 }
 function increase(x) {
     x.style.height = "100px";
@@ -17,11 +19,16 @@ function decrease(x) {
     x.style.height = "70px";
     x.style.width = "70px";
 }
-function showPage(shown, hidden, hidden2, hidden3) {
+
+
+function showPage(shown, hidden, hidden2, hidden3,hidden4,hidden5,hidden6) {
   document.getElementById(shown).style.display='block';
   document.getElementById(hidden).style.display='none';
   document.getElementById(hidden2).style.display='none';
   document.getElementById(hidden3).style.display='none';
+  document.getElementById(hidden4).style.display='none';
+  document.getElementById(hidden5).style.display='none';
+  document.getElementById(hidden6).style.display='none';
   return false;
 }
 
@@ -240,6 +247,8 @@ data = data || {};
 
     }
 var jarnumber=0;
+var countOfJars = [];
+var storedNames;
 
 //insert text underneath each with CSS coordinates -google how to use text in javascript for html
 
@@ -249,8 +258,9 @@ function addHealth(){
     if (jarnumber==1){
     document.getElementById("jar1").style.visibility = "visible";
 	document.getElementById("titleJar1").innerHTML = 'Health';
-	
     }
+	
+  
     if (jarnumber==2){;
     document.getElementById("jar2").style.visibility = "visible";
 	document.getElementById("titleJar2").innerHTML = 'Health';
@@ -311,11 +321,9 @@ function addHealth(){
 	alert("Error-Shelves Full");
 	}
 
-	
-	
-    
-    
-}
+
+ 
+ }
 
 function addMisc(){
     jarnumber+=1;
@@ -595,3 +603,90 @@ function addWork(){
 
     
 }
+
+function showImage() {
+	var ddl = document.getElementById("mySelect");
+ var selectedValue = ddl.options[ddl.selectedIndex].value;
+    if (selectedValue == "10" || selectedValue == "20" || selectedValue == "30")
+   { 
+	document.getElementById("jar1").src= "http://s8.postimg.org/u6ssqwv8l/first.png";
+	document.getElementById("Page1").style.display='block';
+  document.getElementById("Home").style.display='none';
+  document.getElementById("Page2").style.display='none';
+  document.getElementById("Page3").style.display='none';
+  document.getElementById("Page4").style.display='none';
+   }
+   if (selectedValue == "40" || selectedValue == "50" || selectedValue == "60")
+   { 
+	document.getElementById("jar1").src= "http://s21.postimg.org/4x2iuu0xz/second.png";
+	document.getElementById("Page1").style.display='block';
+  document.getElementById("Home").style.display='none';
+  document.getElementById("Page2").style.display='none';
+  document.getElementById("Page3").style.display='none';
+  document.getElementById("Page4").style.display='none';
+   }
+   if (selectedValue == "70%" || selectedValue == "80%" || selectedValue == "90%")
+   {
+	document.getElementById("jar1").src= "http://s3.postimg.org/b8ayetqfn/third.png";
+	document.getElementById("Page1").style.display='block';
+  document.getElementById("Home").style.display='none';
+  document.getElementById("Page2").style.display='none';
+  document.getElementById("Page3").style.display='none';
+  document.getElementById("Page4").style.display='none';
+   }
+    if (selectedValue == "100")
+   {
+	 document.getElementById("jar1").src= "http://s11.postimg.org/97uvq5mrn/last.png";
+	   alert("Congratulations-You Finished Your Tasks!");
+	   document.getElementById("Page1").style.display='block';
+  document.getElementById("Home").style.display='none';
+  document.getElementById("Page2").style.display='none';
+  document.getElementById("Page3").style.display='none';
+  document.getElementById("Page4").style.display='none';
+   }
+   
+}
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
+
+$("#slideshow1 > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow1 > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow1');
+},  3000);
+
+setInterval(function() { 
+  $('#slideshow2 > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow2');
+},  3000);
+$(document).ready(function(){
+
+  $("#mySelect").change(function(){
+  console.log ( '#change function worked' ); 
+   
+  showImage();  
+  console.log ( '#change function worked' ); 
+  });
+});
+////WHEN SAVING THE JARS TO LOCAL STORAGE YOU ALSO HAVE TO SAVE THE JAR NUMBER
+// SWITCH BETWEEN IMAGES TO FILL UP JAR
+
+
+
